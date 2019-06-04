6a8b7ec40ed3609e54e7ba1e06cc66a9be745df6
Michał Bachta <michalbachta@gmail.com>
2019-06-04 12:24:36 +0200

    app info button

--- src/views/MainMenuToolbar.vue
+++ src/views/MainMenuToolbar.vue
@@ -1,6 +1,12 @@
 <template>
   <v-toolbar app color="orange" dark>
     <v-toolbar-title>Main Menu ;)</v-toolbar-title>
+    
+    <v-spacer/>
+
+    <v-btn icon>
+      <v-icon>info</v-icon>
+    </v-btn>
   </v-toolbar>
 </template>
 
