f7315c1d6fe461f27dbb9d9219dfd30a535cb2cd
Michał Bachta <michalbachta@gmail.com>
2019-06-05 10:48:54 +0200

    Getting cards deck

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -1,15 +1,35 @@
-export class Card {
-    value: CardValue;
-    color: CardColor;
+export function getCardsDeck(startNumber: CardValue): Card[] {
+    const values = Object.keys(CardValue).filter(value => isNaN(Number(value)));
+    const desiredValues = values.slice(startNumber);
 
-    constructor(value: CardValue, color: CardColor) {
+    const colors = Object.keys(CardColor).filter(color => isNaN(Number(color)));
+
+    let cards: Card[] = [];
+
+    for (let i = 0; i < colors.length; i++) {
+        const color = colors[i];
+        for (let i = 0; i < desiredValues.length; i++) {
+            const value = desiredValues[i];
+
+            const card = new Card(value, color);
+            cards.push(card);
+        }
+    }
+
+    return cards;
+}
+
+export class Card {
+    value: string;
+    color: string;
+
+    constructor(value: string, color: string) {
         this.value = value;
         this.color = color;
     }
 }
 
 export enum CardValue {
-    ACE,
     TWO,
     THREE,
     FOUR,
@@ -22,6 +42,7 @@ export enum CardValue {
     JACK,
     QUEEN,
     KING,
+    ACE,
 }
 
 export enum CardColor {
